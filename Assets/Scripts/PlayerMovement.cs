874dcaf37dda7b20feed6c5c28dfbfba1434b17b
Fahmi Ihsan <67350562+rad3nz@users.noreply.github.com>
2024-06-19 10:13:41 +0700

    add character movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, 0f);
+
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
